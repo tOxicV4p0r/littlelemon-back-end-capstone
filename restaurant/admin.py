11ed62a6f9090a1fd71ebb0aa500ec31f5223699
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-24 07:05:30 +0700

    remove dup

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Menu
-from .models import Booking
+from .models import Menu, Booking
 
 # Register your models here.
 admin.site.register(Menu)
